f8577d2d31e9145011bedfd662be79e620633635
65160219 <152172067+65160219@users.noreply.github.com>
2024-07-23 02:35:51 +0700

    create component in image menu

--- src/app/components/Image.tsx
+++ src/app/components/Image.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import '../css/imgMenuStyle.css';
+import { IconPlus, IconSearch } from '@tabler/icons-react';
 
+const listImage: React.FC = () => {
+  let image = [
+    { id: 1, name: 'Image 1' },
+    { id: 2, name: 'Image 2' },
+    { id: 3, name: 'Image 3' },
+    { id: 4, name: 'Image 4' },
+    { id: 5, name: 'Image 5' },
+    { id: 6, name: 'Image 6' },
+    { id: 7, name: 'Image 7' },
+    { id: 8, name: 'Image 8' },
+    { id: 10, name: 'Image 10' },
+    { id: 11, name: 'Image 11' },
+    { id: 12, name: 'Image 12' },
+    { id: 13, name: 'Image 13' },
+    { id: 14, name: 'Image 14' },
+  ];
 
-const Image: React.FC = () => {
+  function clickAddList() {
+    image.push({ id: 15, name: 'Image 15' });
+  }
+
   return (
-    <div className="template-tool">
-      <h2>Img Tool</h2>
+    <div className="bgMargin">
+      <div className="search-input-container">
+        <IconSearch className="search-icon" />
+        <input type="text" placeholder="   Search" className="search-input"></input>
+      </div>
+      <div className="allImgArea">
+        <div className="grid-container">
+          {image.map(img => (
+            <div key={img.id} className="grid-item">
+              {img.name}
+            </div>
+          ))}
+        </div>
+      </div>
+      
+      <button className="btnPlus" onClick={() => clickAddList()}><IconPlus className="plus-icon"></IconPlus></button>
     </div>
   );
 };
 
-export default Image;
+export default listImage;

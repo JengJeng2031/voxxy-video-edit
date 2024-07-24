3faea50a8395642cb053044720623de1e713068c
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2024-07-24 13:42:33 +0700

    save button

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,11 +2,16 @@
 
 import React from 'react';
 import '../css/header.css'; 
+import Dropdown from '../components/Dropdown';
 const Header: React.FC = () => {
   return (
     <header className="header">
       <h1>My Header</h1>
+      <div className='Export_btn'>
+        <Dropdown/>
+      </div>
     </header>
+    
   );
 };
 
